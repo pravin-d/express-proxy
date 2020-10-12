c9ec6219b3e4611fc7cabd7865a354b5924fd71b
Pravin D <probablypravin@gmail.com>
2020-10-12 18:59:05 +0530

    Adding cors headers

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
-var proxy = require('express-http-proxy');
+const proxy = require('express-http-proxy');
+const cors = require('cors')
 const app = express()
 const port = process.env.port || 8080;
 const proxyPath = process.env.proxyPath || '/';
 const proxyTarget = process.env.proxyTarget || 'https://api.hubapi.com';
- 
+
+
+app.use(cors());
 app.use(proxyPath, proxy(proxyTarget));
 
 app.listen(port, () => {
